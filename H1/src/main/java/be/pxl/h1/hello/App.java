73a94eb5eb970b465d55a4d835e5adb170d3bb96
Sam Vanderstraeten <sam.vanderstraeten@pxl.be>
2023-11-07 09:26:30 +0100

    Added Hello World example H1

---
+++ H1/src/main/java/be/pxl/h1/hello/App.java
@@ -0,0 +1,17 @@
+package be.pxl.h1.hello;
+
+public class App {
+    public static void main(String[] args) {
+
+        // Een lijn in commentaar
+
+        /*
+        Commentaar in blok
+        Zal niet worden uitgevoerd
+        Meerdere regels
+        */
+
+        System.out.println("Hallo allemaal!");
+
+    }
+}
