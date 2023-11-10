77e4cf7c92dbd167db6ee0d99c3fa54a34c017e0
Sam Vanderstraeten <sam.vanderstraeten@pxl.be>
2023-11-10 16:18:21 +0100

    Commented out stuff in H7 FileHandler start code

--- H7/src/main/java/be/pxl/h7/oefening3/FileHandler.java
+++ H7/src/main/java/be/pxl/h7/oefening3/FileHandler.java
@@ -36,24 +36,24 @@ public class FileHandler {
 //        }
 //        return null;
 //    }
-
-    public static void writeHighscore(Result result) {
-        if(!Files.exists(PATH)) {
-            try {
-                Files.createDirectory(Paths.get("files"));
-                Files.createFile(PATH);
-            } catch (IOException e) {
-                System.err.println("Error creating file: " + e.getMessage());
-            }
-        }
-
-        try {
-            BufferedWriter writer = Files.newBufferedWriter(PATH);
-            writer.write(result.toFile());
-            writer.close();
-        } catch (IOException e) {
-            System.err.println("Error creating file: " + e.getMessage());
-        }
-
-    }
+//
+//    public static void writeHighscore(Result result) {
+//        if(!Files.exists(PATH)) {
+//            try {
+//                Files.createDirectory(Paths.get("files"));
+//                Files.createFile(PATH);
+//            } catch (IOException e) {
+//                System.err.println("Error creating file: " + e.getMessage());
+//            }
+//        }
+//
+//        try {
+//            BufferedWriter writer = Files.newBufferedWriter(PATH);
+//            writer.write(result.toFile());
+//            writer.close();
+//        } catch (IOException e) {
+//            System.err.println("Error creating file: " + e.getMessage());
+//        }
+//
+//    }
 }
